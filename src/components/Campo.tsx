6c9ddf96be00df77c87236fe02be1c7e1f470df5
moradan <realmoradan@hotmail.com>
2024-05-02 09:42:42 -0300

    refactor <Campo .../>: extraigo el componente React

---
+++ src/components/Campo.tsx
@@ -0,0 +1,8 @@
+export function Campo( {id, etiqueta}: {id: string, etiqueta: string} ) {
+    return (
+        <div>
+            <label htmlFor={id}>{etiqueta}</label>
+            <input id={id} className="form-control bg-transparent" />
+        </div>
+    );
+}
--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -1,28 +1,27 @@
+import { Campo } from "./Campo";
+
 export default function Formulario() {
-    return (
-        <form>
-            <div className='row'>
-                <div className='col'>
-                    <label htmlFor='CampoFecha'>Fecha</label>
-                    <input id='CampoFecha' className='form-control bg-transparent' />
-                </div>
-                <div>
-                    <label htmlFor='CampoLocalia'>Localía</label>
-                    <input id='CampoLocalia' className='form-control bg-transparent' />
-                </div>
-                <div>
-                    <label htmlFor='CampoHorario'>Horario</label>
-                    <input id='CampoHorario' className='form-control bg-transparent' />
-                </div>
-                <div>
-                    <label htmlFor='CampoAdversario'>Adversario</label>
-                    <input id='CampoAdversario' className='form-control bg-transparent' />
-                </div>
-                <div>
-                    <label htmlFor='CampoCategoria'>Categoría</label>
-                    <input id='CampoCategoria' className='form-control bg-transparent' />
-                </div>
-            </div>
-        </form>
-    );
+  return (
+    <form>
+      <div className="row">
+        <Campo id="CampoFecha" etiqueta="Fecha" />
+        <div>
+          <label htmlFor="CampoLocalia">Localía</label>
+          <input id="CampoLocalia" className="form-control bg-transparent" />
+        </div>
+        <div>
+          <label htmlFor="CampoHorario">Horario</label>
+          <input id="CampoHorario" className="form-control bg-transparent" />
+        </div>
+        <div>
+          <label htmlFor="CampoAdversario">Adversario</label>
+          <input id="CampoAdversario" className="form-control bg-transparent" />
+        </div>
+        <div>
+          <label htmlFor="CampoCategoria">Categoría</label>
+          <input id="CampoCategoria" className="form-control bg-transparent" />
+        </div>
+      </div>
+    </form>
+  );
 }
