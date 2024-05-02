3466098598a9bf76c8b3495d5abcfb91c348089b
moradan <realmoradan@hotmail.com>
2024-05-02 10:58:50 -0300

    primer bosquejo de la estructura de la presentacion del flyer

--- src/components/Presentacion.tsx
+++ src/components/Presentacion.tsx
@@ -1,6 +1,56 @@
 export default function Presentacion() {
     return (
-        <div className="col">
-        </div>
+        <>
+            <header>
+                <h2>
+                    Interclubes 2024
+                </h2>
+            </header>
+            <ul className="list-group-flush p-0">
+                <li className="list-group-item">
+                    <header>
+                        <h3>
+                            Primera Fecha
+                        </h3>
+                    </header>
+                    <section>
+                        <h4>Local</h4>
+                        <ul className="list-group-flush p-0">
+                            <li className="list-group-item">
+                                <div className="row">
+                                    <div className="col-2">
+                                        11:00
+                                    </div>
+                                    <div className="col-5">
+                                        tenis club argentino-A
+                                    </div>
+                                    <div className="col-5">
+                                        caballeros +70 primera-A
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </section>
+                    <section>
+                        <h4>Visitante</h4       >
+                        <ul className="list-group-flush p-0">
+                            <li className="list-group-item">
+                                <div className="row">
+                                    <div className="col-2">
+                                        09:00
+                                    </div>
+                                    <div className="col-5">
+                                        centro deportivo huracan-A
+                                    </div>
+                                    <div className="col-5">
+                                        varones sub 14 nivel B
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </section>
+                </li>
+            </ul>
+        </>
     );
 }
