cf246ee806df4294bccec2bc7896f10eb6992ee5
moradan <realmoradan@hotmail.com>
2024-05-02 17:58:13 -0300

    arranged the layout of the form to make better use of space

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -3,12 +3,22 @@ import { Campo } from "./Campo";
 export default function Formulario() {
   return (
     <>
-      <div className="row g-1 mb-3">
+      <div className="row g-2 mb-3">
+        <div className="col-6">
           <Campo id="CampoFecha" type="date" etiqueta="Fecha" />
+        </div>
+        <div className="col-6">
           <Campo id="CampoLocalia" type="text" etiqueta="Localía" />
+        </div>
+        <div className="col-4">
           <Campo id="CampoHorario" type="time" etiqueta="Horario" />
+        </div>
+        <div className="col-8">
           <Campo id="CampoAdversario" type="text" etiqueta="Adversario" />
+        </div>
+        <div className="col">
           <Campo id="CampoCategoria" type="text" etiqueta="Categoría" />
+        </div>
       </div>
       <div className="row g-1 mb-3">
         <div className="col-6">
