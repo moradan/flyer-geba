3b0eb9d1ca7fc32f6d352f8b0033767caaf3364c
moradan <realmoradan@hotmail.com>
2024-05-02 11:18:54 -0300

    extraigo el ElementoFecha que contiene toda la estructura de presentacion de una fecha del torneo

--- src/components/Presentacion.tsx
+++ src/components/Presentacion.tsx
@@ -1,3 +1,5 @@
+import { ElementoFecha } from "./ElementoFecha";
+
 export default function Presentacion() {
     return (
         <>
@@ -8,47 +10,10 @@ export default function Presentacion() {
             </header>
             <ul className="list-group-flush p-0">
                 <li className="list-group-item">
-                    <header>
-                        <h3>
-                            Primera Fecha
-                        </h3>
-                    </header>
-                    <section>
-                        <h4>Local</h4>
-                        <ul className="list-group-flush p-0">
-                            <li className="list-group-item">
-                                <div className="row">
-                                    <div className="col-2">
-                                        11:00
-                                    </div>
-                                    <div className="col-5">
-                                        tenis club argentino-A
-                                    </div>
-                                    <div className="col-5">
-                                        caballeros +70 primera-A
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </section>
-                    <section>
-                        <h4>Visitante</h4       >
-                        <ul className="list-group-flush p-0">
-                            <li className="list-group-item">
-                                <div className="row">
-                                    <div className="col-2">
-                                        09:00
-                                    </div>
-                                    <div className="col-5">
-                                        centro deportivo huracan-A
-                                    </div>
-                                    <div className="col-5">
-                                        varones sub 14 nivel B
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </section>
+                    <ElementoFecha />
+                </li>
+                <li className="list-group-item">
+                    <ElementoFecha />
                 </li>
             </ul>
         </>
