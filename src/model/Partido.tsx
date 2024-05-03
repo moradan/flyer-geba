7f17ab904969c0b0b7c138311466d874f87ab276
moradan <realmoradan@hotmail.com>
2024-05-03 19:35:28 -0300

    defined the object to hold data about partidos.

--- src/model/Partido.tsx
+++ src/model/Partido.tsx
@@ -1,1 +1,7 @@
-export class Partido { }
+export class Partido {
+    fecha: string = "01/01/1970";
+    localia: string = "Local";
+    horario: string = "00:00";
+    adversario: string = "";
+    categoria: string = "";
+}
