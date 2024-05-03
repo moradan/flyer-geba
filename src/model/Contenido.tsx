9606077fc7b16ebaa1e093f4aa6bf43d99853b50
moradan <realmoradan@hotmail.com>
2024-05-03 19:38:21 -0300

    added an implementation of List to the Contenido class to manage the list of tennis matches

--- src/model/Contenido.tsx
+++ src/model/Contenido.tsx
@@ -1,37 +1,40 @@
 import { ElementoFecha } from "@/components/ElementoFecha";
 import { Partido } from "./Partido";
+import { ReactNode } from "react";
+import { IPresentable } from "./IPresentable";
+import * as Collections from "typescript-collections"
 
 export class Contenido implements IPresentable, IModificable<Partido> {
     
+    listaPartidos = new Collections.LinkedList<Partido>;
+
     agregar(partido: Partido): void {
+        this.listaPartidos.add(partido);
         return;
     }
     
     borrar(partido: Partido): void {
+        this.listaPartidos.remove(partido);
         return;
     }
     
-    presentacion(): any {
+    presentacion(): ReactNode {
         return <ContenidoFormateado />;
     }
 }
 
-function ContenidoFormateado() {
+function ContenidoFormateado(): ReactNode {
     return (
         <>
-            <header>
-                <h2 className="text-center mb-3">
-                    Interclubes 2024
-                </h2>
-            </header>
-            <ul className="list-group-flush p-0">
-                <li className="list-group-item mb-3">
-                    <ElementoFecha fecha="Primera Fecha" />
-                </li>
-                <li className="list-group-item mb-3">
-                    <ElementoFecha fecha="Segunda Fecha" />
-                </li>
-            </ul>
         </>
     );
 }
+
+// <ul className="list-group-flush p-0">
+//     <li className="list-group-item mb-3">
+//         <ElementoFecha fecha="Primera Fecha" />
+//     </li>
+//     <li className="list-group-item mb-3">
+//         <ElementoFecha fecha="Segunda Fecha" />
+//     </li>
+// </ul>
