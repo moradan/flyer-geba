7e1212886880d0a6f53414a4eaa157b72cc760f3
moradan <realmoradan@hotmail.com>
2024-05-14 17:05:32 -0300

    agrego el fondo al flyer para empezar el proceso de formatear

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,14 +29,15 @@ export default function Home() {
         <meta name="description" content="App para generar un flyer con los partidos de las fechas del mes." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <div className="container col-12 col-sm-9 col-md-8 col-lg-7 col-xl-6 pt-3">
-          <nav className="row g-1 mb-5">
+      <div className="container col-12 col-sm-9 col-md-8 col-lg-7 col-xl-6">
+          <nav className="row g-1 mt3- mb-5">
             <PanelControl />
           </nav>
           <header className="col mb-5">
             <Formulario listaPartidos={listaPartidos} actualizador={setListaPartidos} clave={clave}/>
           </header>
-          <main className="col d-flex flex-column align-items-center">
+          <main className="position-relative col d-flex flex-column align-items-center">
+            <div className="fondo" />
             <Flyer listaPartidos={listaPartidos}/>
           </main> 
       </div>
