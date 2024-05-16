50cd7a5c0f9766ba743c04cbdd04fbadfde0731d
moradan <realmoradan@hotmail.com>
2024-05-16 16:29:58 -0300

    formateo los titulares de cada fecha para imitar el flyer muestra

--- src/components/flyer/EncabezadoFecha.tsx
+++ src/components/flyer/EncabezadoFecha.tsx
@@ -1,12 +1,13 @@
 import formatDate from "@/utils/formatDate";
 
-export default function EncabezadoFecha(
-    {fecha}:
-    {fecha: Date}
-) {
-    return (
-        <header>
-            <h3 className="text-center">{formatDate(fecha)}</h3>
-        </header>
-    );
+export default function EncabezadoFecha({ fecha }: { fecha: Date }) {
+  return (
+    <header>
+      <h3 className="text-center">
+        <span className="bg-primary-subtle px-1 rounded">
+          {formatDate(fecha)}
+        </span>
+      </h3>
+    </header>
+  );
 }
