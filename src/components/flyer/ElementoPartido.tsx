069bbf5cc7d88dbb7c02b5f1853210a62ffa1917
moradan <realmoradan@hotmail.com>
2024-05-20 13:36:47 -0300

    agrego mayusculas automaticas a los elementos partido del flyer mediante css

--- src/components/flyer/ElementoPartido.tsx
+++ src/components/flyer/ElementoPartido.tsx
@@ -4,7 +4,7 @@ import formatTime from "@/utils/formatTime";
 export default function ElementoPartido({ partido }: { partido: Partido }) {
   return (
     <li className="list-group-item px-2">
-      <div className="row gx-1">
+      <div className="row gx-1 text-uppercase">
         <div className="col-2">{formatTime(partido.horario)}</div>
         <div className="col-5">{partido.adversario}</div>
         <div className="col-5 text-end">{partido.categoria}</div>
