ada39fae5da7d20152fc5e9cdcaf4ce22f02d115
moradan <realmoradan@hotmail.com>
2024-05-23 18:44:25 -0300

    agrego text-nowrap:

--- src/components/flyer/ElementoPartido.tsx
+++ src/components/flyer/ElementoPartido.tsx
@@ -4,7 +4,7 @@ import formatTime from "@/utils/formatTime";
 export default function ElementoPartido({ partido }: { partido: Partido }) {
   return (
     <li className="list-group-item px-2">
-      <div className="row fs-7 fw-bold gx-1 text-uppercase">
+      <div className="row fs-7 fw-bold gx-1 text-uppercase text-nowrap">
         <div className="col-1">{formatTime(partido.horario)}</div>
         <div className="col-6">{partido.adversario}</div>
         <div className="col-5 text-end">{partido.categoria}</div>
