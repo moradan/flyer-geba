067a94c7d131011ef7183b5901ad2658c4ee906b
moradan <realmoradan@hotmail.com>
2024-06-05 08:42:44 -0300

    Parche: cambio la imagen de junio por la de mayo

--- src/components/flyer/Fondo.tsx
+++ src/components/flyer/Fondo.tsx
@@ -1,9 +1,9 @@
 export default function Fondo() {
   return (
     <img
-      src="/fondo.jpg"
-      alt="imagen de las canchas de GEBA"
-      className="position-absolute z-n1 tamanio-fondo"
+      src='/fondo-mayo.jpg'
+      alt='imagen de las canchas de GEBA'
+      className='position-absolute z-n1 tamanio-fondo'
     />
   );
 }
