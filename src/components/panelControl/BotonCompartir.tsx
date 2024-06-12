8f8720e9d49b9f92523cd6e19d695d5ae95df434
moradan <realmoradan@hotmail.com>
2024-06-12 18:05:36 -0300

    Muestro y escondo el orden de partidos.
    Cuando voy a generar la imagen escondo el div que contiene el orden de partido.
    Cuando la imagen termina de descargarse vuelvo a mostrarlos.

--- src/components/panelControl/BotonCompartir.tsx
+++ src/components/panelControl/BotonCompartir.tsx
@@ -1,7 +1,6 @@
 import { MouseEvent, useState } from "react";
 import Boton from "../Boton";
 import { toJpeg } from "html-to-image";
-import { Options } from "html-to-image/lib/types";
 
 export default function BotonCompartir() {
   function manejarClick(e: MouseEvent) {
@@ -11,12 +10,18 @@ export default function BotonCompartir() {
       throw new Error("No hay un nodo en el documento HTML con id flyer-nodo.");
     }
 
-    toJpeg(nodoFlyer).then((dataUrl) => {
-      const link = document.createElement("a");
-      link.download = "flyer.jpg";
-      link.href = dataUrl;
-      link.click();
-    });
+    ocultarUI();
+
+    toJpeg(nodoFlyer)
+      .then(descargarImagen)
+      .then(mostrarUI);
+  }
+
+  function descargarImagen(dataUrl: string) {
+    const link = document.createElement("a");
+    link.download = "flyer.jpg";
+    link.href = dataUrl;
+    link.click();
   }
 
   return (
@@ -29,3 +34,18 @@ export default function BotonCompartir() {
     </>
   );
 }
+
+function ocultarUI() {
+  const elementosIndice = document.querySelectorAll(".partido-index");
+  for(const elemento of elementosIndice) {
+    elemento.classList.add("d-none");
+  }
+}
+
+function mostrarUI(value: void): void | PromiseLike<void> {
+  const elementosIndice = document.querySelectorAll(".partido-index");
+  for(const elemento of elementosIndice) {
+    elemento.classList.remove("d-none");
+  }
+}
+
