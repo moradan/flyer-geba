39277a828c4dcd74d148c062b58f9a246bab1bea
moradan <realmoradan@hotmail.com>
2024-06-18 16:46:41 -0300

    Boton abrir
    Cuando haces click en Abrir uso un prompt para pedir el nombre del flyer a abrir.
    Cambio el localstorage key="buffer" para que contenga el nombre del flyer que queremos abrir.
    Fuerzo reload, lo que a su vez hace que se cargue el contenido del flyer desde localstorage.

--- src/components/panelControl/BotonAbrir.tsx
+++ src/components/panelControl/BotonAbrir.tsx
@@ -1,5 +1,15 @@
 import Boton from "../Boton";
 
-export default function BotonAbrir() {
-  return <Boton caption="Abrir" identifier="BotonAbrir" />;
+export default function BotonAbrir({ variant }: {variant: string}) {
+  return <Boton variant={variant} clickHandler={manejarClick} caption="Abrir" identifier="BotonAbrir" />;
+}
+
+function manejarClick() {
+  const respuesta = prompt("cual flyer queres abrir");
+  
+  if(respuesta) {
+    localStorage.setItem("buffer", respuesta);
+  }
+  
+  location.reload();
 }
