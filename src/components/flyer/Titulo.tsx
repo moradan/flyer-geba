a0a6382ad3fb0b6656615732d4159b042e49d2d7
moradan <realmoradan@hotmail.com>
2025-03-04 11:18:25 -0300

    cambio a la estetica azul

--- src/components/flyer/Titulo.tsx
+++ src/components/flyer/Titulo.tsx
@@ -3,11 +3,9 @@ import { Logo } from "./Logo";
 export default function Titulo({ titulo }: { titulo: string }) {
   return (
     <div className="row align-items-center justify-content-center">
-      <Logo />
-      <h1 className="fs-2 col-6 text-center p-2 bg-primario text-white text-shadow rounded">
+      <h1 className="fs-2 col-6 text-center p-2 text-white text-shadow rounded">
         {titulo}
       </h1>
-      <Logo />
     </div>
   );
 }
