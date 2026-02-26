2462b2a5ea82614a1e4b768dfdfc576565d182e1
moradan <realmoradan@hotmail.com>
2026-02-26 16:41:41 -0300

    modifico css para que la foto de fondo se vea completa aunque el flyer este vacio

--- src/components/flyer/Fondo.tsx
+++ src/components/flyer/Fondo.tsx
@@ -5,7 +5,7 @@ export default function Fondo() {
     <>
       <Image
         src='/fondo-febrero-2026.jpeg'
-        alt='fondo azul liso'
+        alt='fondo azul con marca de agua de GEBA'
         className='position-absolute z-n1 tamanio-fondo'
       />
     </>
