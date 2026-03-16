23483cc6ab8ea9805401447747cd35c6a66d9dca
moradan <realmoradan@hotmail.com>
2026-03-15 22:20:32 -0300

    Muevo el titulo a la parte superior del panel de control. Esta imagen me parece visualmente mejor organizada.

--- src/components/panelControl/PanelControl.tsx
+++ src/components/panelControl/PanelControl.tsx
@@ -8,14 +8,14 @@ import ExcelReader from "./ExcelReader";
 export default function PanelControl() {
   return (
     <nav className="row g-2">
+			<div>
+				<h1 className="text-center">Nombre del flyer: {AdministradorDatos.nombreFlyer}</h1>
+			</div>
       <BotonCompartir variant="col-12 col-md-6" />
       <BotonGuardar variant="col-12 col-md-6" />
       <BotonAbrir variant="col-12 col-md-6" />
       <ExcelReader />
       <BotonImportar variant="col-12 col-md-6" />
-			<div>
-				<h1 className="text-center">Nombre del flyer: {AdministradorDatos.nombreFlyer}</h1>
-			</div>
     </nav>
   );
 }
