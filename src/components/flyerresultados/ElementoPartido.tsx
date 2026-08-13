e43ecf98836c9297909d52956521f9405a2a1bdf
moradan <realmoradan@hotmail.com>
2026-08-13 12:27:13 -0300

    Sigo corrijiendo el formato del panfleto de resultados.

--- src/components/flyerresultados/ElementoPartido.tsx
+++ src/components/flyerresultados/ElementoPartido.tsx
@@ -14,13 +14,13 @@ export default function ElementoPartido({ partido }: { partido: Partido }) {
 
   return (
     <li className='list-group-item px-2 partido-elemento text-shadow'>
-      <div className='row align-items-center fs-7 fw-bold gx-0 text-uppercase text-nowrap'>
+      <div className='row align-items-center fs-7 fw-bold gx-1 text-uppercase text-nowrap'>
         <div className='col-4 text-end' style={{ fontSize }}>
           {partido.categoria}
         </div>
-				<div className='col-2'><CampoResultado /></div>
-				<div className='col-2'><CampoResultado /></div>
-        <div className='col-4 ps-3 pb-2' style={{ fontSize }}>
+				<div className='col-2 p-1'><CampoResultado /></div>
+				<div className='col-2 p-1'><CampoResultado /></div>
+        <div className='col-4' style={{ fontSize }}>
           {partido.adversario}
         </div>
       </div>
