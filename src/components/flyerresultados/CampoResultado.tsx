4ea1582f6b11e484ee5708234b3076f7d7af9b88
moradan <realmoradan@hotmail.com>
2026-08-19 11:26:31 -0300

    quito la longitud maxima del campo resultado para permitir utilizar emoji.

--- src/components/flyerresultados/CampoResultado.tsx
+++ src/components/flyerresultados/CampoResultado.tsx
@@ -4,7 +4,6 @@ export default function CampoResultado() {
 	return (
 		<Form.Control 
 			type="text" 
-			maxLength={1} 
 			className="border-0 rounded-0 text-shadow text-center fs-3 font-tahoma"
 		/>
 	);
