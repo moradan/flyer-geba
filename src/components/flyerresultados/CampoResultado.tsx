484bf05def46f063eeecbcd808bc8e24514d84da
moradan <realmoradan@hotmail.com>
2026-08-20 20:03:19 -0300

    Disminuyo el tamanio de la letra del campo de resultados para que los emoji no se vean tapados por el borde de la caja(?).

--- src/components/flyerresultados/CampoResultado.tsx
+++ src/components/flyerresultados/CampoResultado.tsx
@@ -4,7 +4,7 @@ export default function CampoResultado() {
 	return (
 		<Form.Control 
 			type="text" 
-			className="border-0 rounded-0 text-shadow text-center fs-3 font-tahoma"
+			className="border-0 rounded-0 text-shadow text-center fs-4 font-tahoma"
 		/>
 	);
 }
